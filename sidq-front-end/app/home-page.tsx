e3f288dff38c3e9428dbaf0161bcddbd8b2fc0e2
Syed A <63989020+Cront@users.noreply.github.com>
2025-04-07 21:38:58 -0700

    Setting up file

--- sidq-front-end/app/home-page.tsx
+++ sidq-front-end/app/home-page.tsx
@@ -1,1 +1,5 @@
 // start working on home page with taskbar and such
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
