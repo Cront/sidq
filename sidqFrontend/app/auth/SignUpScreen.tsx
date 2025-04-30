b68d7829e80e6d82ac96f855edad9d3146e78dbc
Syed A <63989020+Cront@users.noreply.github.com>
2025-04-29 23:57:36 -0700

    Adding textboxes to input Org info to add to db

--- sidqFrontend/app/auth/SignUpScreen.tsx
+++ sidqFrontend/app/auth/SignUpScreen.tsx
@@ -1,18 +1,38 @@
-import { Image, Text, StyleSheet, View, Dimensions } from "react-native";
+import {
+  Image,
+  Text,
+  StyleSheet,
+  View,
+  Dimensions,
+  TextInput,
+} from "react-native";
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "expo-router";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
-const scaleFactor = screenWidth / 430;
+const scaleFactor = screenWidth / 393;
 
 export default function SignUpScreen() {
+  const [organizationName, setOrganizationName] = useState("");
+
   return (
     <View style={styles.container}>
+      {/* Sidq logo */}
       <Image
         source={require("@/assets/images/sidqLogo.png")}
         style={styles.image}
+      />
+
+      {/* Create Account Header */}
+      <Text style={styles.header}>Create Your Organization's Account</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Organization Name"
+        value={organizationName}
+        onChangeText={setOrganizationName}
       />
     </View>
   );
@@ -26,11 +46,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   image: {
-    width: 90 * scaleFactor,
-    height: 85 * scaleFactor,
+    width: 150 * scaleFactor,
+    height: 125 * scaleFactor,
     resizeMode: "cover",
     position: "absolute",
-    top: screenHeight * 0.065,
-    left: (screenWidth - 90) / 2,
+    top: screenHeight * 0.08,
+    left: screenWidth / 2 - (150 * scaleFactor) / 2,
+  },
+  header: {
+    fontSize: 32 * scaleFactor,
+    fontWeight: 800,
+    textAlign: "center",
+    position: "absolute",
+    top: screenHeight * 0.3,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    fontSize: 16,
   },
 });
