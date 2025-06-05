2a1312931fce57a93609de72a88c341785947426
Syed A <63989020+Cront@users.noreply.github.com>
2025-06-05 16:22:36 -0500

    Added user model

--- sidq-backend/app/models.py
+++ sidq-backend/app/models.py
@@ -3,6 +3,28 @@ from datetime import datetime, timezone
 from .extensions import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(80), nullable=False, unique=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(512), nullable=False)
+
+    address = db.Column(db.String(225), nullable=True, unique=False)
+    phone_number = db.Column(db.String(225), nullable=True, unique=True)
+
+    preferred_currency = db.Column(db.String(80), nullable=False, unique=False)
+    timezone = db.Column(db.String(80), nullable=False, unique=False)
+
+    # TODO: gamification & donations related
+
+    email_verified = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(
+        timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+
 class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
@@ -10,24 +32,29 @@ class Organization(db.Model):
     email = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(512), nullable=False)
 
-    address = db.Column(db.String(225), nullable=True, unique=True) # mybe orgs could have same address
+    # mybe orgs could have same address
+    address = db.Column(db.String(225), nullable=True, unique=True)
     phone_number = db.Column(db.String(225), nullable=True)
     website_link = db.Column(db.String(225), nullable=True, unique=True)
 
     org_description = db.Column(db.Text, nullable=True)
 
-    # media storing 
+    # media storing
     file_name = db.Column(db.String(225), nullable=True)
-    media_data = db.Column(db.LargeBinary, nullable=True)  # assuming storing a logo or document
-    media_type = db.Column(db.String(100), nullable=True) # TODO: make sure to handle extracting file type from user uploaded media
+    # assuming storing a logo or document
+    media_data = db.Column(db.LargeBinary, nullable=True)
+    # TODO: make sure to handle extracting file type from user uploaded media
+    media_type = db.Column(db.String(100), nullable=True)
 
-    created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    created_at = db.Column(db.DateTime(timezone=True),
+                           default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(
+        timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
     # return organization name for debugging purposes
     def __repr__(self):
         return f"Organization {self.name}"
-    
+
     def to_json(self):
         return {
             "id": self.id,
