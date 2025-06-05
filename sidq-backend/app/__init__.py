63cde747253a8021b741aecfd8f2281526f9f4e1
Syed A <63989020+Cront@users.noreply.github.com>
2025-06-05 17:02:58 -0500

    Added user bp to init file

--- sidq-backend/app/__init__.py
+++ sidq-backend/app/__init__.py
@@ -1,11 +1,10 @@
-from app.routes.organization_routes import organization_bp
 from flask import Flask
 from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 
 from .config import Config
 from .extensions import db, migrate
+from .routes.organization_routes import organization_bp
+from .routes.user_routes import user_bp
 
 
 def create_app():
@@ -13,12 +12,12 @@ def create_app():
     app.config.from_object(Config)
 
     app.register_blueprint(organization_bp, url_prefix='/organization')
+    app.register_blueprint(user_bp, url_prefix='/user')
 
     CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .models import Organization
+    from .models import Organization, User
 
     return app
-
