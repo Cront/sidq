7605ab5b2214c53d51ce03c11cb8f7df457cef6a
Syed A <63989020+Cront@users.noreply.github.com>
2025-06-24 18:31:39 -0400

    Logo, welcome msg, and google button showing up

--- sidq-frontend/app/auth/LogInScreen.tsx
+++ sidq-frontend/app/auth/LogInScreen.tsx
@@ -1,5 +1,88 @@
-import { View } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  Dimensions,
+  Platform,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import GoogleSignInButton from "./GoogleSignInButton";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+
+const scaleFactor = screenWidth / 393;
 
 export default function LogInScreen() {
-  return <View></View>;
+  const redirectUri = AuthSession.makeRedirectUri({
+    useProxy: true,
+  });
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "165374973540-fevinpcp24ec316erocbregddq86smsv.apps.googleusercontent.com",
+    redirectUri,
+  });
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          style={{ backgroundColor: "white", flex: 1 }}
+          contentContainerStyle={styles.container}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Image
+            source={require("@/assets/images/sidqLogo.png")}
+            style={styles.image}
+          />
+
+          <Text style={styles.welcomeBack}>Welcome Back</Text>
+
+          {/* Google sign-in */}
+          <GoogleSignInButton
+            onPress={() => promptAsync()}
+            disabled={!request}
+            style={styles.googleSignIn}
+          />
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    alignItems: "center",
+    paddingTop: screenHeight * 0.065,
+    paddingBottom: screenHeight * 0.12,
+  },
+  image: {
+    width: 100 * scaleFactor,
+    height: 85 * scaleFactor,
+    resizeMode: "contain",
+    marginBottom: 20,
+  },
+  welcomeBack: {
+    fontSize: 32 * scaleFactor,
+    fontWeight: "700",
+    textAlign: "center",
+    fontFamily: "Inter",
+    marginBottom: 24,
+  },
+  googleSignIn: {
+    borderWidth: 1,
+    borderRadius: 100,
+    paddingHorizontal: 40,
+  },
+});
