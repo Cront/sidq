f79a9cdfc1adefff934588789ef846f6210780ce
Syed A <63989020+Cront@users.noreply.github.com>
2025-06-30 15:02:15 -0500

    Fixed import issue

--- sidq-backend/app/routes/login_routes.py
+++ sidq-backend/app/routes/login_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from werkzeug import security
 
-from models import Organization, User
+from ..models import Organization, User
 
 login_bp = Blueprint('login_bp', __name__)
 
