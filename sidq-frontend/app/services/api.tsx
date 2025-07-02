6c9864675ef3790355034806ed9c15a9a5b78102
Syed A <63989020+Cront@users.noreply.github.com>
2025-07-02 17:57:07 -0500

    loginUser with axios and SecureStore

--- sidq-frontend/app/services/api.tsx
+++ sidq-frontend/app/services/api.tsx
@@ -1,14 +1,28 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
+import * as SecureStore from "expo-secure-store";
 
 const API_URL = "http://127.0.0.1:5000";
 
 export const loginUser = async (email: string, password: string) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
-  const { access_token, refresh_token } = response.data;
+  try {
+    const response = await axios.post(`${API_URL}/login`, {
+      email,
+      password,
+    });
 
-  await AsyncStorage.setItem("access_token", access_token);
-  await AsyncStorage.setItem("refresh_token", refresh_token);
+    const { access_token, refresh_token, user } = response.data;
 
-  return response.data.user;
+    await SecureStore.setItemAsync("access_token", access_token);
+    await SecureStore.setItemAsync("refresh_token", refresh_token);
+
+    return user;
+  } catch (error: unknown) {
+    if (!isAxiosError(error)) {
+      const message =
+        error.response?.data?.message || "Login failed. Please try again.";
+      throw new Error(message);
+    } else {
+      throw new Error("Network error. Check your connection and try again.");
+    }
+  }
 };
