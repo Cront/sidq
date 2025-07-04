f43a507ce69b5ee7a73ae5d17d33ce8c113bb48f
Syed A <63989020+Cront@users.noreply.github.com>
2025-07-03 23:49:45 -0500

    Fixed hardcoded secret

--- sidq-backend/app/config.py
+++ sidq-backend/app/config.py
@@ -1,9 +1,19 @@
+import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://sidq_dev:786Y3liS!dq@localhost:5432/sidq_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("Missing DATABASE_URL in environment variables.")
+
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    if not JWT_SECRET_KEY:
+        raise ValueError("Missing JWT_SECRET_KEY in environment variables.")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = "YaAli786!"  # TODO: update to random string for security
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=6)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=14)
